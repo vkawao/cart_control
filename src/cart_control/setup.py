880d6418d6d9c8a17a8fd7bc5c7777510127af9b
vkawao <wikornearth.par@gmail.com>
2026-03-25 16:31:51 +0700

    First Commit

---
+++ src/cart_control/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'cart_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kawao',
+    maintainer_email='kawao@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'modbus_node = cart_control.modbus_node:main'
+        ],
+    },
+)
